7be7342aeda0de8cdd7c0a6d28e050956d413658
José Bahamonde Cordeiro <jose@bahamonde.dev>
2020-01-06 00:44:59 +0100

    fix: Ahora tamén se esconde o borde cando se arranca un cliente maximizado

--- contents/code/main.js
+++ contents/code/main.js
@@ -2,3 +2,7 @@ workspace.clientMaximizeSet.connect(function(client, h, v) {
     client.noBorder = h && v;
 });
 
+workspace.clientAdded.connect(function(client) {
+    var area = workspace.clientArea(KWin.MaximizeArea, client);
+    client.noBorder = client.width >= area.width && client.height >= area.height;
+});
