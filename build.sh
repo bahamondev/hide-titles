a41c892aff7d6a84aac52aeb1f26cf1a521551ec
bahamondev <jose@bahamonde.dev>
2020-04-29 19:06:28 +0200

    feat: Information messages in build and install scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+echo "Cleaning..."
 rm -f hide-titles.kwinscript
+echo "Building..."
 zip hide-titles.kwinscript -r contents LICENSE metadata.desktop
--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-./build.sh && plasmapkg2 -t kwinscript -i hide-titles.kwinscript
+./build.sh 
+echo "Removing previous installation..."
+plasmapkg2 -t kwinscript -r hide-titles.kwinscript
+echo "Installing script..."
+plasmapkg2 -t kwinscript -i hide-titles.kwinscript
+echo "Done!"
