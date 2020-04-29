fe6724975a931920148942cdb8b4ab87c8bc2b08
bahamondev <jose@bahamonde.dev>
2020-04-29 19:09:56 +0200

    feat: Added directory cleaning after installation

--- install.sh
+++ install.sh
@@ -4,4 +4,6 @@ echo "Removing previous installation..."
 plasmapkg2 -t kwinscript -r hide-titles.kwinscript
 echo "Installing script..."
 plasmapkg2 -t kwinscript -i hide-titles.kwinscript
+echo "Deleting remaining files..."
+rm -f hide-titles.kwinscript
 echo "Done!"
